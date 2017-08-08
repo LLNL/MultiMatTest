729811c0388814226c2e2bbac30ad4eab0256d44
Matt Martineau <m.martineau@hotmail.co.uk>
2017-08-08 11:57:37 -0500

    Changed all of the data allocations to use CUDA managed memory. Started to refactor out the individual routines and write device code for them.

---
+++ Makefile
@@ -0,0 +1,24 @@
+GCC_FLAGS = -O3 -std=c99
+NVCC_FLAGS = -O3 -arch=sm_35 -Xcompiler "-O3" -ccbin g++
+
+ifeq ($(DEBUG), yes)
+  GCC_FLAGS_DEBUG = -O0 -g -std=c99
+  NVCC_FLAGS_DEBUG = -O0 -g -arch=sm_35 -Xcompiler "-O0 -g" -ccbin g++
+endif
+
+CSRC  = $(wildcard *.c)
+CXXSRC  = $(wildcard *.cu)
+OBJS = $(patsubst %.c, %.o, $(CSRC))
+OBJS += $(patsubst %.cu, %.o, $(CXXSRC))
+
+mm: $(OBJS)
+	nvcc $(NVCC_FLAGS) $(OBJS) -o multi
+
+%.o: %.c Makefile
+	gcc $(GCC_FLAGS) -c $< -o $@
+
+%.o: %.cu Makefile
+	nvcc $(NVCC_FLAGS) -c $< -o $@
+
+clean:
+	rm -rf *.o multi
---
+++ Shared.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stdio.h>
+
+#define NTHREADS 128
+
+// http://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
+#define gpu_check(ans)                                                         \
+  { gpu_assert((ans), __FILE__, __LINE__); }
+inline void gpu_assert(cudaError_t code, const char *file, int line,
+                       bool abort = true) {
+  if (code != cudaSuccess) {
+    fprintf(stderr, "GPU Error Returned: %s %s %d\n", cudaGetErrorString(code),
+            file, line);
+    if (abort)
+      exit(code);
+  }
+}
+
+void finish_sum_reduce(int nblocks1, double *reduce_array, double *result);
